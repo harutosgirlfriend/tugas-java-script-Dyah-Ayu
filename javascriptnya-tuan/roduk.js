40c7d333a052ec046ea25124a25e428f9d7ee504
harutosgirlfriend <dyahayuetikawati@gmail.com>
2024-06-03 00:09:41 +0700

    first commit

--- javascriptnya-tuan/roduk.js
+++ javascriptnya-tuan/roduk.js
@@ -3,3 +3,18 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-bar').onclick=() =>{
     navbar.classList.toggle('active');
 }
+let total = 0;
+
+function addToCart(namaproduk, harga) {
+    total += harga;
+    document.getElementById("total").textContent = total.toFixed(2);
+    
+    const li = document.createElement("li");
+    li.textContent = `${namaproduk} - ${harga.toFixed(2)}`;
+    document.getElementById("keranjang-item").appendChild(li);
+}
+
+function checkout() {
+    document.getElementById("bayar-dengan").classList.remove("hidden");
+}
+
---
+++ pembayaran.js
@@ -0,0 +1,8 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active');
+}
+function melengkapipembayaran() {
+    alert("terimakasih sudah membeli di toko kami");
+    
+}
